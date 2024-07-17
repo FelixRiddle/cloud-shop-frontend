c182aff3d748d9039dd1a8a29a8fc78ba2397b48
Felix <fedecraft3312@gmail.com>
2024-07-16 21:01:52 -0300

    Header component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * Header
+ */
+export default function Header() {
+	return (
+		<header className="barra">
+			<div className="contenedor">
+				<h1>CRM - Client administration</h1>
+			</div>
+		</header>
+	);
+}
