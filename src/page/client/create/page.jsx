656e8889dbf805ccad7c9929260ae9e88394e59c
Felix <fedecraft3312@gmail.com>
2024-07-18 16:21:44 -0300

    Validate data

--- src/page/client/create/page.jsx
+++ src/page/client/create/page.jsx
@@ -1,36 +1,116 @@
+import { useRef, useState } from "react";
+
 /**
  * Create client form
  */
 export default function CreateClientPage() {
+	const form = useRef(null);
+	const [client, setClient] = useState({
+		name: "",
+		surname: "",
+		company: "",
+		phoneNumber: "",
+	});
+	
+	/**
+	 * Handle change
+	 */
+	function handleChange(e) {
+		setClient({
+			...client,
+			[e.target.name]: e.target.value
+		});
+	}
+	
+	/**
+	 * Validate client
+	 */
+	function validateClient(e) {
+		// Every field is required
+		for(const [key, value] of Object.entries(client)) {
+			if(!value) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Create client
+	 */
+	function handleCreateClient(e) {
+		
+	}
+	
 	return (
 		<>
 			<h2>New client</h2>
 			
-			<form>
+			<form ref={form}>
 				<legend>Fill fields and submit to create a client</legend>
 				
 				<div className="campo">
 					<label htmlFor="name">Name</label>
-					<input type="text" name="name" id="name" placeholder="Name" />
+					<input
+						type="text"
+						name="name"
+						id="name"
+						placeholder="Name"
+						onChange={handleChange}
+					/>
 				</div>
 				
 				<div className="campo">
 					<label htmlFor="surname">Surname</label>
-					<input type="text" name="surname" id="surname" placeholder="Surname" />
+					<input
+						type="text"
+						name="surname"
+						id="surname"
+						placeholder="Surname"
+						onChange={handleChange}
+					/>
 				</div>
 				
 				<div className="campo">
 					<label htmlFor="company">Company</label>
-					<input type="text" name="company" id="company" placeholder="Company" />
+					<input
+						type="text"
+						name="company"
+						id="company"
+						placeholder="Company"
+						onChange={handleChange}
+					/>
+				</div>
+				
+				<div className="campo">
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="Email"
+						onChange={handleChange}
+					/>
 				</div>
 				
 				<div className="campo">
 					<label htmlFor="phoneNumber">Phone number</label>
-					<input type="number" name="phoneNumber" id="phoneNumber" placeholder="Phone number" />
+					<input
+						type="number"
+						name="phoneNumber"
+						id="phoneNumber"
+						placeholder="Phone number"
+						onChange={handleChange}
+					/>
 				</div>
 				
 				<div className="campo">
-					<button className="btn btn-azul">Create client</button>
+					<button
+						className="btn btn-azul"
+						disabled={!validateClient()}
+						onClick={handleCreateClient}
+					>Create client</button>
 				</div>
 			</form>
 		</>
