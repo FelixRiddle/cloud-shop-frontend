48e1479821ed1ab5342e190c88560017de7bde15
Felix <fedecraft3312@gmail.com>
2024-07-18 10:04:36 -0300

    Show client information

--- src/page/client/Client.jsx
+++ src/page/client/Client.jsx
@@ -1,4 +1,3 @@
-
 /**
  * Client
  */
@@ -6,8 +5,23 @@ export default function Client({
 	client
 }) {
 	return (
-		<div>
-			
-		</div>
+		<li className="cliente">
+			<div className="info-cliente">
+				<p className="nombre">{client.name}</p>
+				<p className="empresa">{client.company}</p>
+				<p>{client.email}</p>
+				<p>Phone: {client.phoneNumber}</p>
+			</div>
+			<div className="acciones">
+				<a href="#" className="btn btn-azul">
+					<i className="fas fas-pen-alt"></i>
+					Edit client
+				</a>
+				<button className="btn btn-rojo btn-eliminar">
+					<i className="fas fa-times"></i>
+					Delete client
+				</button>
+			</div>
+		</li>
 	);
 }
