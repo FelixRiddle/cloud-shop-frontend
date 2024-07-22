70756c47fa35bdd4122fdddf77813deea7227316
Felix <fedecraft3312@gmail.com>
2024-07-21 23:15:45 -0300

    Don't show navigation bar on login

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { Link } from "react-router-dom";
+import { UserContext } from '../context/UserContext';
 
 /**
  * Navigation
  */
 export default function Navigation() {
+	const [user, setUser] = useContext(UserContext);
+	
+	if(!user || !user._id) {
+		return null;
+	}
+	
 	return (
 		<aside className="sidebar col-3">
 			<h2>Administration</h2>
